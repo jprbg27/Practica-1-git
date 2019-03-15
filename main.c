995b4d7b039b6586164fcee8834eb7fb83c3a1bf
Juan <juan.pablo.cruz@aptiv.com>
2019-03-15 09:54:02 -0600

    Este es mi primer Commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define VARIABLE 0
+#define VARIABLE 2
+#define VARIABLE 3
+#define VARIABLE 5
+
+/*Ya hay cambios
+*/
+
+#define PATH "proc\kmsg"
+int main (void)
+{
+	return 0;
+}
+
+/*se agrego una libreria  
+Juan Pablo cruz 15 marzo 2019*/
