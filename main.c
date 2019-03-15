44be5ec810ade0fe19c27f07383178a23307154d
Juan <juan.pablo.cruz@aptiv.com>
2019-03-15 11:39:18 -0600

    Segundo Requisito

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define VARIABLE 2
 #define VARIABLE 3
 
 /*Ya hay cambios
 */
-
-
+int resta(int a, int b);
 void main (void)
 {
 	printf("Hello World");
